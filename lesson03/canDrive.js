1fc941dbcd56b98eef3990f03ab9457376346211
Ogonna Anunoby <ogonna1994@hotmail.com>
2026-06-20 17:42:11 -0400

    Completed assignment

---
+++ lesson03/canDrive.js
@@ -0,0 +1,14 @@
+const firstName = "John"
+const lastName = "Smith"
+const driverAge = 40
+const drivingAge = 16
+
+if(driverAge >= drivingAge)
+{
+  console.log(`${firstName} ${lastName} can drive and has driven for ${driverAge - drivingAge} years.`)
+}
+else
+{
+  for(let i = 5; i >= 0; i--)
+    console.log(i)
+}
