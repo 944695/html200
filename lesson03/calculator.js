824be7c8049354264da81ade2fc709e32a34fbff
Ogonna Anunoby <ogonna1994@hotmail.com>
2026-06-20 08:15:40 -0400

    Completed assignment

---
+++ lesson03/calculator.js
@@ -0,0 +1,64 @@
+let number1 = parseInt(prompt("Enter the first number: "))
+let number2 = parseInt(prompt("Enter the second number: "))
+let number3 = parseInt(prompt("Enter the third number: "))
+let number4 = parseInt(prompt("Enter the fourth number: "))
+
+console.log(`${number1} + ${number2} = ${number1 + number2}`)
+console.log(`${number1} - ${number2} = ${number1 - number2}`)
+console.log(`${number1} * ${number2} = ${number1 * number2}`)
+console.log(`${number1} / ${number2} = ${number1 / number2}`)
+
+console.log(`${number1} + ${number3} = ${number1 + number3}`)
+console.log(`${number1} - ${number3} = ${number1 - number3}`)
+console.log(`${number1} * ${number3} = ${number1 * number3}`)
+console.log(`${number1} / ${number3} = ${number1 / number3}`)
+
+console.log(`${number1} + ${number4} = ${number1 + number4}`)
+console.log(`${number1} - ${number4} = ${number1 - number4}`)
+console.log(`${number1} * ${number4} = ${number1 * number4}`)
+console.log(`${number1} / ${number4} = ${number1 / number4}`)
+
+console.log(`${number2} + ${number1} = ${number2 + number1}`)
+console.log(`${number2} - ${number1} = ${number2 - number1}`)
+console.log(`${number2} * ${number1} = ${number2 * number1}`)
+console.log(`${number2} / ${number1} = ${number2 / number1}`)
+
+console.log(`${number2} + ${number3} = ${number2 + number3}`)
+console.log(`${number2} - ${number3} = ${number2 - number3}`)
+console.log(`${number2} * ${number3} = ${number2 * number3}`)
+console.log(`${number2} / ${number3} = ${number2 / number3}`)
+
+console.log(`${number2} + ${number4} = ${number2 + number4}`)
+console.log(`${number2} - ${number4} = ${number2 - number4}`)
+console.log(`${number2} * ${number4} = ${number2 * number4}`)
+console.log(`${number2} / ${number4} = ${number2 / number4}`)
+
+console.log(`${number3} + ${number1} = ${number3 + number1}`)
+console.log(`${number3} - ${number1} = ${number3 - number1}`)
+console.log(`${number3} * ${number1} = ${number3 * number1}`)
+console.log(`${number3} / ${number1} = ${number3 / number1}`)
+
+console.log(`${number3} + ${number2} = ${number3 + number2}`)
+console.log(`${number3} - ${number2} = ${number3 - number2}`)
+console.log(`${number3} * ${number2} = ${number3 * number2}`)
+console.log(`${number3} / ${number2} = ${number3 / number2}`)
+
+console.log(`${number3} + ${number4} = ${number3 + number4}`)
+console.log(`${number3} - ${number4} = ${number3 - number4}`)
+console.log(`${number3} * ${number4} = ${number3 * number4}`)
+console.log(`${number3} / ${number4} = ${number3 / number4}`)
+
+console.log(`${number4} + ${number1} = ${number4 + number1}`)
+console.log(`${number4} - ${number1} = ${number4 - number1}`)
+console.log(`${number4} * ${number1} = ${number4 * number1}`)
+console.log(`${number4} / ${number1} = ${number4 / number1}`)
+
+console.log(`${number4} + ${number2} = ${number4 + number2}`)
+console.log(`${number4} - ${number2} = ${number4 - number2}`)
+console.log(`${number4} * ${number2} = ${number4 * number2}`)
+console.log(`${number4} / ${number2} = ${number4 / number2}`)
+
+console.log(`${number4} + ${number3} = ${number4 + number3}`)
+console.log(`${number4} - ${number3} = ${number4 - number3}`)
+console.log(`${number4} * ${number3} = ${number4 * number3}`)
+console.log(`${number4} / ${number3} = ${number4 / number3}`)
