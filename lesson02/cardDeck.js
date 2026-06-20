44352cb01a714ffd0951f394a21a400b1844f3c7
Ogonna Anunoby <ogonna1994@hotmail.com>
2026-06-20 09:28:21 -0400

    Completed assignment

---
+++ lesson02/cardDeck.js
@@ -0,0 +1,6 @@
+faces = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+for(face of faces)
+  for(suit of suits)
+    console.log(face + " of " + suit)
